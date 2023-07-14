cf85921c5e72730077b2aabe2e2175acf587632d
Mehmet Can Buga <bugamehmetcan@gmail.com>
2023-07-14 08:19:52 +0300

    Update rotate.js

--- rotate.js
+++ rotate.js
@@ -1,6 +1,6 @@
 doc
   .save() // Save the current state of the document
-  .translate(startX + 10, startY + 145) // Set the starting point
+  .translate( x, y ) // Set the starting point (x , y)
   .rotate(-90, { origin: [0, 0] }) // Rotate the text at a specific angle
   .font('Roboto-Bold.ttf')
   .fontSize('12')
